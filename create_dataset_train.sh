30cd7446874898a491a17ed6b8600cb6e2a07099
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-01-19 00:19:13 -0500

    Create create_dataset_train.sh

---
+++ create_dataset_train.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=def-mpederso       
+#SBATCH --cpus-per-task=6                
+#SBATCH --gres=gpu:1                     
+#SBATCH --mem=25G                        
+#SBATCH --time=1:00:00               
+
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURM_TMPDIR
+echo "SLURM_ARRAYID="$SLURM_ARRAYID
+echo "SLURM_ARRAYID"=$SLURM_ARRAYID
+echo "SLURM_ARRAY_JOB_ID"=$SLURM_ARRAY_JOB_ID
+echo "SLURM_ARRAY_TASK_ID"=$SLURM_ARRAY_TASK_ID
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+cp train.tar.gz $SLURM_TMPDIR
+tar -xf $SLURM_TMPDIR/train.tar.gz -C $SLURM_TMPDIR
+rm $SLURM_TMPDIR/train.tar.gz
+cd $SLURM_TMPDIR/train
+ls
+
+cd $SLURM_SUBMIT_DIR
+
+export IMAGENET_DIR=$SLURM_TMPDIR
+export WRITE_DIR=$SLURM_TMPDIR
+
+./write_imagenet.sh 256 1.0 90
+cp $SLURM_TMPDIR/train_256_1.0_90.ffcv $SLURM_SUBMIT_DIR
+
